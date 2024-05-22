14de8045acd1bf724e868eff192479d28fd7f950
jspnguyen <theantisine@gmail.com>
2024-05-22 10:50:24 -0700

    Added ldl sdk key

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import gradio, llama_index, boto3
+import gradio, llama_index, boto3, ldclient, os
 from llama_index.retrievers.bedrock import AmazonKnowledgeBasesRetriever
 from llama_index.core import get_response_synthesizer
 from llama_index.llms.bedrock.base import Bedrock
+from ldclient.config import Config
+from dotenv import load_dotenv
+
+s3_client = boto3.client('s3')
+
+# load_dotenv()
+# ldclient.set_config(Config(os.getenv("ldl_sdk_key")))
+# client = ldclient.get()
 
 retriever = AmazonKnowledgeBasesRetriever(
-    knowledge_base_id="<knowledge-base-id>",
+    knowledge_base_id="YZXPI4CEXE",
     retrieval_config={
         "vectorSearchConfiguration": {
             "numberOfResults": 4,
@@ -14,11 +22,17 @@ retriever = AmazonKnowledgeBasesRetriever(
     },
 )
 
-query = "How big is Milky Way as compared to the entire universe?"
+query = """
+User prompt: 
+What is a poisson distribution?
+"""
+
+# ! Line below is erroring out, please save us!
 retrieved_results = retriever.retrieve(query)
 
 # Prints the first retrieved result
 print(retrieved_results[0].get_content())
+print("--------------------------")
 
 llm = Bedrock(model="anthropic.claude-v2", temperature=0, max_tokens=3000)
 response_synthesizer = get_response_synthesizer(
@@ -26,3 +40,6 @@ response_synthesizer = get_response_synthesizer(
 )
 response_obj = response_synthesizer.synthesize(query, retrieved_results)
 print(response_obj)
+
+# if __name__ == "__main__":
+#     main()
